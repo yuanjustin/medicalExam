a6be3df42d41b489b5fb8553b0cc040943d66ea7
yuanjustin <ywz1994@163.com>
2017-05-24 16:56:16 +0300

    Update cardListModel.java

--- src/main/java/com/mycompany/medicalExam/model/cardListModel.java
+++ src/main/java/com/mycompany/medicalExam/model/cardListModel.java
@@ -2,4 +2,8 @@ package com.mycompany.medicalExam.model;
 
 public class cardListModel
 {
+  List<detailModel> cardListTab =new arryList();
+  cardListModel(Data data, int sys){
+  //data find information
+  }
 }
