ff75180a538d51ee88306be818074ca854a38e01
yuanjustin <ywz1994@163.com>
2017-05-24 12:37:35 +0300

    Update MainActivity.java

--- src/main/java/com/mycompany/medicalExam/MainActivity.java
+++ src/main/java/com/mycompany/medicalExam/MainActivity.java
@@ -2,13 +2,17 @@ package com.mycompany.medicalExam;
 
 import android.app.*;
 import android.os.*;
-
+import android.support.v7.widget.Toolbar;
 public class MainActivity extends Activity 
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+                // Adding Toolbar to Main screen
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
