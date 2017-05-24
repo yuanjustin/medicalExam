58a05836a1521290b198fb79203bc535b3d54e6e
yuanjustin <ywz1994@163.com>
2017-05-24 17:21:24 +0300

    Update data.java

--- src/main/java/com/mycompany/medicalExam/bean/data.java
+++ src/main/java/com/mycompany/medicalExam/bean/data.java
@@ -2,4 +2,20 @@ package com.mycompany.medicalExam.bean;
 
 public class data
 {
+    List<systemsBean> systems = new ArrayList<>();
+   List<methodsBean> systems = new ArrayList<>();
+  List<detailBean> systems = new ArrayList<>();
+  data(){
+    //ini
+    getjson();
+  }  
+  void getjson(){
+  //github
+  }
+  void json2obj(){
+  //todo
+  }
+  
+  
+  
 }
