c2d0689c92afecd4f518ebf2f288c98f5e81be91
yuanjustin <ywz1994@163.com>
2017-05-24 14:53:58 +0300

    Update MainActivity.java

--- src/main/java/com/mycompany/medicalExam/MainActivity.java
+++ src/main/java/com/mycompany/medicalExam/MainActivity.java
@@ -1,17 +1,25 @@
 package com.mycompany.medicalExam;
-
-import android.os.*;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-public class MainActivity extends AppCompatActivity 
-{
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-                // Adding Toolbar to Main screen
+        setContentView(R.layout.activity_main);
+        // Adding Toolbar to Main screen
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
@@ -19,4 +27,5 @@ public class MainActivity extends AppCompatActivity
         tabs.addTab(tabs.newTab().setText("Tab 2"));
         tabs.addTab(tabs.newTab().setText("Tab 3"));
     }
+
 }
