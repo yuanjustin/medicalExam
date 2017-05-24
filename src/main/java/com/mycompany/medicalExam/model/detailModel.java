1148a79d430a09ef6719468b45494c2bdb793b7d
yuanjustin <ywz1994@163.com>
2017-05-24 17:01:26 +0300

    Update detailModel.java

--- src/main/java/com/mycompany/medicalExam/model/detailModel.java
+++ src/main/java/com/mycompany/medicalExam/model/detailModel.java
@@ -2,4 +2,7 @@ package com.mycompany.medicalExam.model;
 
 public class detailModel
 {
+  void getDetail(){}
+  void getBrift(){}
+ // https://stackoverflow.com/questions/2116162/how-to-display-html-in-textview
 }
