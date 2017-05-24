da6040e709603198f3fb3beaf776ad7c360fc1d3
yuanjustin <ywz1994@163.com>
2017-05-24 17:00:13 +0300

    Update methodListModel.java

--- src/main/java/com/mycompany/medicalExam/model/methodListModel.java
+++ src/main/java/com/mycompany/medicalExam/model/methodListModel.java
@@ -2,4 +2,8 @@ package com.mycompany.medicalExam.model;
 
 public class methodListModel
 {
+    List<cardListModel> methodListTab =new arryList();
+  methodListModel(Data data, int sys){
+  //data find information
+  }
 }
