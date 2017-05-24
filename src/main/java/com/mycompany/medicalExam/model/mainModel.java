d2b21bb8771f4cc30d4f61fa308672814045c040
yuanjustin <ywz1994@163.com>
2017-05-24 16:39:09 +0300

    Update mainModel.java

--- src/main/java/com/mycompany/medicalExam/model/mainModel.java
+++ src/main/java/com/mycompany/medicalExam/model/mainModel.java
@@ -1,5 +1,17 @@
 package com.mycompany.medicalExam.model;
 
-public class mainModel
+public class mainModel extends Model
 {
+  String title;
+  int SystemIndex;
+  int MethodIndex;
+  public int cardIndex;
+  void ini(){
+  //title index =1
+    title="";
+    SystemIndex=1;
+    MethodIndex=1;
+    cardIndex=1;
+    
+  }
 }
