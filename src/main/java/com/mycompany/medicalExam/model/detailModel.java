290ba20ba8b645f8b8f9b5854aae2cad082b524e
yuanjustin <ywz1994@163.com>
2017-05-24 17:08:13 +0300

    Update detailModel.java

--- src/main/java/com/mycompany/medicalExam/model/detailModel.java
+++ src/main/java/com/mycompany/medicalExam/model/detailModel.java
@@ -2,7 +2,7 @@ package com.mycompany.medicalExam.model;
 
 public class detailModel
 {
-  void getDetail(){}
-  void getBrift(){}
+  void getDetail(Data data, int sys,int meth, int card){}
+  void getBrift(Data data, int sys,int meth, int card){}
  // https://stackoverflow.com/questions/2116162/how-to-display-html-in-textview
 }
